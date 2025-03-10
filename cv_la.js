327c08d45d78c09376d44231c17e15706f4a322f
Ludovic Aubert <ludo.aubert@gmail.com>
2025-03-10 15:51:57 +0100

    Add files via upload

--- cv_la.js
+++ cv_la.js
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS field(
   iddiagram INTEGER DEFAULT 1,
   PRIMARY KEY (iddiagram, idfield),
   name VARCHAR(128),
-  importance INTEGER,
+  stars INTEGER,
   idbox INTEGER,
   FOREIGN KEY (iddiagram) REFERENCES diagram(iddiagram) ON DELETE CASCADE,
   FOREIGN KEY (iddiagram, idbox) REFERENCES box(iddiagram, idbox) ON DELETE CASCADE,
@@ -55,57 +55,54 @@ INSERT INTO tag(type_code, code) VALUES
 ('COLOR','chartreuse'),('COLOR','DarkOrange'),('COLOR','BlueViolet'),('COLOR','MediumSeaGreen'),('COLOR','LightSeaGreen'),('COLOR','BurlyWood');
 
 
-WITH cte(box_title, field_name, importance) AS (
-	SELECT 'Languages', 'C++', 35 UNION ALL
-	SELECT 'Languages', 'SQL', 35 UNION ALL
- 	SELECT 'Languages', 'JSON', 10 UNION ALL
-  	SELECT 'Languages', 'JS', 30 UNION ALL
-	SELECT 'Languages', 'Html', 10 UNION ALL
-	SELECT 'Languages', 'SVG', 10 UNION ALL
- 	SELECT 'Languages', 'Css', 10 UNION ALL
-	SELECT 'Languages', 'Python', 5 UNION ALL
+WITH cte(box_title, field_name, stars) AS (
+	SELECT 'Languages', 'C++', 5 UNION ALL
+	SELECT 'Languages', 'SQL', 5 UNION ALL
+ 	SELECT 'Languages', 'JSON', 4 UNION ALL
+  	SELECT 'Languages', 'JS', 4 UNION ALL
+	SELECT 'Languages', 'Html', 4 UNION ALL
+	SELECT 'Languages', 'SVG', 4 UNION ALL
+ 	SELECT 'Languages', 'Css', 4 UNION ALL
+	SELECT 'Languages', 'Python', 3 UNION ALL
 	SELECT 'Languages', 'regexp', 5 UNION ALL
-	SELECT 'Maths', 'Graphs', 30 UNION ALL
-	SELECT 'Maths', 'Algebra', 30 UNION ALL
-	SELECT 'Maths', 'Statistics', 25 UNION ALL
-	SELECT 'Maths', 'Geometry', 20 UNION ALL
-	SELECT 'Tools', 'SQL Server', 40 UNION ALL
-	SELECT 'Tools', 'PostgreSQL', 20 UNION ALL
-	SELECT 'Tools', 'PGLite', 10 UNION ALL
-	SELECT 'Tools', 'Oracle', 10 UNION ALL
-	SELECT 'Tools', 'NodeJS', 20 UNION ALL
-	SELECT 'Tools', 'Web Browsers', 30 UNION ALL
-	SELECT 'Tools', 'GIT', 10 UNION ALL
-	SELECT 'Skills', 'Endurance', 20 UNION ALL
-	SELECT 'Skills', 'Vision', 15 UNION ALL
-	SELECT 'Skills', 'Persistence', 30 UNION ALL
-	SELECT 'Skills', 'Research', 30 UNION ALL
-	SELECT 'Skills', 'Curiosity', 35 UNION ALL
-	SELECT 'Skills', 'Dependable', 25 UNION ALL
-	SELECT 'Skills', 'Abstraction', 20 UNION ALL
-	SELECT 'Skills', 'Practical', 15 UNION ALL
-	SELECT 'Experience', 'Queries', 30 UNION ALL
-	SELECT 'Experience', 'Functional', 30 UNION ALL
-	SELECT 'Experience', 'Information Structure', 30 UNION ALL
-	SELECT 'Experience', 'Multi Language', 30 UNION ALL
-	SELECT 'Experience', 'Regression Monitoring', 30 UNION ALL
-	SELECT 'Experience', 'Simplicity', 30 UNION ALL
-	SELECT 'Experience', 'Integrity', 30 UNION ALL
-	SELECT 'Experience', 'Speed', 30 UNION ALL
-	SELECT 'Education', 'Maths', 30 UNION ALL
-	SELECT 'Education', 'Computer Science', 30 UNION ALL
-	SELECT 'Education', 'Ecole Centrale', 30 UNION ALL
-	SELECT 'International', 'USA', 20 UNION ALL
-	SELECT 'International', 'Germany', 30 UNION ALL
-	SELECT 'International', 'Switzerland', 15
+	SELECT 'Maths', 'Graphs', 5 UNION ALL
+	SELECT 'Maths', 'Algebra', 4 UNION ALL
+	SELECT 'Maths', 'Statistics', 4 UNION ALL
+	SELECT 'Maths', 'Geometry', 3 UNION ALL
+	SELECT 'Tools', 'SQL Server', 5 UNION ALL
+	SELECT 'Tools', 'PostgreSQL', 4 UNION ALL
+	SELECT 'Tools', 'PGLite', 4 UNION ALL
+	SELECT 'Tools', 'Oracle', 3 UNION ALL
+	SELECT 'Tools', 'NodeJS', 3 UNION ALL
+	SELECT 'Tools', 'Web Browsers', 5 UNION ALL
+	SELECT 'Tools', 'GIT', 4 UNION ALL
+	SELECT 'Skills', 'Endurance', 5 UNION ALL
+	SELECT 'Skills', 'Vision', 4 UNION ALL
+	SELECT 'Skills', 'Persistence', 5 UNION ALL
+	SELECT 'Skills', 'Research', 4 UNION ALL
+	SELECT 'Skills', 'Curiosity', 5 UNION ALL
+	SELECT 'Skills', 'Dependable', 5 UNION ALL
+	SELECT 'Skills', 'Abstraction', 4 UNION ALL
+	SELECT 'Skills', 'Practical', 4 UNION ALL
+	SELECT 'Experience', 'Queries', 5 UNION ALL
+	SELECT 'Experience', 'Functional', 4 UNION ALL
+	SELECT 'Experience', 'Information Structure', 4 UNION ALL
+	SELECT 'Experience', 'Multi Language', 4 UNION ALL
+	SELECT 'Experience', 'Regression Monitoring', 5 UNION ALL
+	SELECT 'Experience', 'Simplicity', 5 UNION ALL
+	SELECT 'Experience', 'Integrity', 4 UNION ALL
+	SELECT 'Experience', 'Speed', 4 UNION ALL
+	SELECT 'International', 'USA', 4 UNION ALL
+	SELECT 'International', 'Germany', 5 UNION ALL
+	SELECT 'International', 'Switzerland', 3
 ), cte_box AS (
 	INSERT INTO box(title)
 	SELECT DISTINCT box_title
 	FROM cte
 	RETURNING *
 )
-INSERT INTO field(idbox, name, importance)
-SELECT cte_box.idbox, cte.field_name, cte.importance
+INSERT INTO field(idbox, name, stars)
+SELECT cte_box.idbox, cte.field_name, cte.stars
 FROM cte
 JOIN cte_box ON cte.box_title = cte_box.title;
 `;
@@ -115,67 +112,28 @@ window.main = async function main()
 	const rt1 = await db.exec(schema);
 	const rt2 = await db.exec(data);
 
-	const Radius = 100;
-
 	const ret = await db.query(`
-		WITH cte AS (
-			SELECT *, SUM(importance) OVER(PARTITION BY idbox ORDER BY idfield
-				ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING) AS running_total,
-				SUM(importance) OVER(PARTITION BY idbox) AS total,
-				ROW_NUMBER() OVER(PARTITION BY idbox ORDER BY idfield) AS idcolor
+		WITH cte_values(val) AS (
+			VALUES(1),(2),(3),(4),(5),(6)
+		), cte_field(idbox, idfield, name, state) AS (
+			SELECT *,
+				CASE WHEN stars >=val THEN 'full' ELSE 'empty' END AS state
 			FROM field
-		), cte2 AS (
-			SELECT *, (2*${Radius}*3.14*importance/total)::numeric(10,2) AS dasharray,
-				(2*${Radius}*3.14*coalesce(running_total)/total)::numeric(10,2) AS dashoffset,
-				importance * 100 / total AS percentage,
-				(2*${Radius} + 1.5*${Radius}*cos((COALESCE(running_total,0)+importance/2)*2*3.14/total))::numeric(10,2) AS x,
-				(2*${Radius} + 1.5*${Radius}*sin((COALESCE(running_total,0)+importance/2)*2*3.14/total))::numeric(10,2) AS y
-			FROM cte
-		), cte_split AS (
-			SELECT idfield, x, unnest(string_to_array("name", ' ')) mot
-			FROM cte2
-		), cte3(idfield, html) AS (
-			SELECT idfield, STRING_AGG(FORMAT('<tspan x="%1$s" dy="%2$s">%3$s</tspan>', x, '1.2em', mot), '')
-			FROM cte_split
-			GROUP BY idfield,x
-		), cte4(idbox, "order", html) AS (
-			SELECT idbox, 1, FORMAT('<svg id="svg%1$s" height="%2$s" width="%2$s">', idbox, '${4*Radius}')
-			FROM box
-
-			UNION ALL
-
-			SELECT idbox, 2, FORMAT('
-				<circle r="${Radius}" cx="${2*Radius}" cy="${2*Radius}"
-        				stroke="%1$s"
-					stroke-width="${2*Radius}"
-        				stroke-dasharray="%2$s %3$s"
-        				stroke-dashoffset="%4$s"/>',
-				code, --%1
-				dasharray, --%2
-				(3.14*2*${Radius})::numeric(10,2), --%3
-				-coalesce(dashoffset,0)) --%4
-			FROM cte2
-			JOIN tag ON type_code='COLOR' AND idcolor=idtag
-
-			UNION ALL
-
-			SELECT idbox, 3, FORMAT('<text x="%1$s" y="%2$s">%3$s</text>', cte2.x, y-20, cte3.html)
-			FROM cte2
-			JOIN cte3 ON cte3.idfield = cte2.idfield
-
-			UNION ALL
-
-			SELECT idbox, 4, FORMAT('<text x="%1$s" y="%1$s"><tspan x="%1$s" dy="%2$s">%3$s</tspan></text>', '${2*Radius}', '0.5em', title)
-			FROM box
-
-			UNION ALL
-
-			SELECT idbox, 5, '</svg>'
-			FROM box
+			CROSS JOIN cte_values
+		), cte_field(idbox, idfield, name,  html) AS (
+			SELECT idbox, idfield, name,
+				STRING_AGG(FORMAT('<input type="checkbox" class="checkbox-round-%1$s" />', state), '\n' ORDER BY val) AS html
+			FROM cte_field
+			GROUP BY idbox, idfield, name
+		), cte_box AS (
+			SELECT idbox, STRING_AGG(FORMAT('<span>%1$s %2$s</span>', name, html) '\n' ORDER BY idfield) AS html
+			FROM cte_field
+			GROUP BY idbox
 		)
-		SELECT STRING_AGG(html, '' ORDER BY idbox, "order") AS html
-		FROM cte4
+		SELECT STRING_AGG(FORMAT('<h2>%1$s</h2>%2$s', title, html) '\n' ORDER BY idbox) AS html
+		FROM cte_box
+		JOIN box ON box.idbox = cte_box.idbox
 	`);
 
-//	document.body.innerHTML = ret.rows[0].html;
+	document.getElementById("left-panel").innerHTML = ret.rows[0].html;
 }
