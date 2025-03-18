75d02e0b10a7d9d878fd2cbcfd5d8535b8524bb6
Ludovic Aubert <ludo.aubert@gmail.com>
2025-03-18 10:37:11 +0100

    Add files via upload

---
+++ vcard_la.js
@@ -0,0 +1,10 @@
+
+
+window.main = async function main()
+{
+	const card = document.getElementById("card");
+	var container = document.getElementById("cards");
+
+	for (var i = 0; i < 10; i++) {
+		container.appendChild(card.cloneNode(true));
+}
